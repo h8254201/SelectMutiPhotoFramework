513deab3b17b0bafb4f8fd7de7c6735861dfc2f4
h8254201 <h82542011@gmail.com>
2018-03-15 16:19:22 +0800

    Initial Commit

---
+++ SelectMutiPhotoFrameWork/SelectMutiPhotoFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  SelectMutiPhotoFrameWork.h
+//  SelectMutiPhotoFrameWork
+//
+//  Created by larvata_ios on 2018/3/15.
+//  Copyright © 2018年 ntubimdbirc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelectMutiPhotoFrameWork.
+FOUNDATION_EXPORT double SelectMutiPhotoFrameWorkVersionNumber;
+
+//! Project version string for SelectMutiPhotoFrameWork.
+FOUNDATION_EXPORT const unsigned char SelectMutiPhotoFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelectMutiPhotoFrameWork/PublicHeader.h>
+
+
